1e09f3f546126e101a705ed07a1605d94cc1f33e
XzAEiT-Murft <143564330+XzAEiT-Murft@users.noreply.github.com>
2026-04-18 17:20:49 -0600

    eliminar dependencia de GUILD_ID y habilitar comandos globales para invitar Engelsik a cualquier servidor

--- deploy-commands.js
+++ deploy-commands.js
@@ -30,17 +30,14 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log('Registrando comandos...');
+        console.log(`Registrando ${commands.length} comandos globales...`);
 
         await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
-            ),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands }
         );
 
-        console.log('Comandos registrados correctamente.');
+        console.log('Comandos globales registrados correctamente.');
     } catch (error) {
         console.error(error);
     }
